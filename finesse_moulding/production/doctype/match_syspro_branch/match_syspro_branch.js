288aec1b8f64af1d3167c5f1e2d3e30ae669bf8a
viclok60 <viclok118@gmail.com>
2024-06-27 12:28:03 +0800

    match syspro branch

---
+++ finesse_moulding/production/doctype/match_syspro_branch/match_syspro_branch.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Victor and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Match Syspro Branch', {
+	// refresh: function(frm) {
+
+	// }
+});
