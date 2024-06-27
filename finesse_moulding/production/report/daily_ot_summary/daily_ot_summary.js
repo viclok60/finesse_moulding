3c5ca00586235037e87a6c72047bb813a26428e3
viclok60 <viclok118@gmail.com>
2024-06-27 15:29:36 +0800

    daily ot summary report

--- finesse_moulding/production/report/daily_ot_summary/daily_ot_summary.js
+++ finesse_moulding/production/report/daily_ot_summary/daily_ot_summary.js
@@ -4,6 +4,49 @@
 
 frappe.query_reports["Daily OT Summary"] = {
 	"filters": [
-
-	]
-};
+        {
+            "fieldname": "from_selected_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today(),
+            "reqd": 1,
+        },
+        {
+            "fieldname": "to_selected_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today(),
+            "reqd": 1,
+        },
+        {
+            "fieldname": "branch",
+            "label": __("Branch"),
+            "fieldtype": "Link",
+            "options": "Branch",
+            "on_change": function() {
+                // Trigger the report refresh when the 'branch' filter changes
+                frappe.query_report.refresh();
+            }
+        },
+    ],
+    // Add a custom method to get the selected 'branch' value and date range
+    get_query: function (filters) {
+        return {
+            filters: [
+                {
+                    fieldname: 'branch',
+                    operator: 'like',
+                    value: filters.branch
+                },
+                {
+                    fieldname: 'date',
+                    operator: 'between',
+                    value: [
+                        filters.from_selected_date,
+                        filters.to_selected_date
+                    ]
+                }
+            ]
+        };
+    }
+};   
