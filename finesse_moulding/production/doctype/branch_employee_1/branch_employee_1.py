3455d49b36abc81d22bb8bdaf6846265d1768c90
viclok60 <viclok118@gmail.com>
2024-06-27 13:44:02 +0800

    daily workforce

---
+++ finesse_moulding/production/doctype/branch_employee_1/branch_employee_1.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Victor and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class BranchEmployee1(Document):
+	pass
