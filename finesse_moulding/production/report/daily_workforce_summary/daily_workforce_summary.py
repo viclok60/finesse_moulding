9b8883ba90cd6a2ad53991420b477ba03ef4cade
Victor Lok <121997329+viclok60@users.noreply.github.com>
2024-07-01 15:37:15 +0800

    Update daily_workforce_summary.py

--- finesse_moulding/production/report/daily_workforce_summary/daily_workforce_summary.py
+++ finesse_moulding/production/report/daily_workforce_summary/daily_workforce_summary.py
@@ -8,6 +8,7 @@ def execute(filters=None):
     selected_branch = filters.get("branch")
     from_selected_date = filters.get("from_selected_date")
     to_selected_date = filters.get("to_selected_date")
+    public_holidays = filters.get("public_holidays", "")
     columns = [
         {"label": "Branch", "fieldname": "branch", "fieldtype": "Data", "width": 90},
         {"label": "Total Days", "fieldname": "total_work_days", "fieldtype": "Data", "width": 90},
@@ -40,17 +41,23 @@ def execute(filters=None):
         columns.insert(1, {"label": "Time In", "fieldname": "time_in", "fieldtype": "Data", "width": 140})
         columns.insert(2, {"label": "Time Out", "fieldname": "time_out", "fieldtype": "Data", "width": 150})
 
-    data = get_data(from_selected_date, to_selected_date, selected_branch)
+    data = get_data(from_selected_date, to_selected_date, selected_branch, public_holidays)
     return columns, data
 
 def is_weekend(date_obj):
     # Check if the day of the week is Saturday (5) or Sunday (6)
     return date_obj.weekday() in [5, 6]
 
-def get_data(from_date, to_date, selected_branch):
+def get_data(from_date, to_date, selected_branch, public_holidays):
     # Convert date strings to datetime objects
     from_date = datetime.strptime(from_date, "%Y-%m-%d")
     to_date = datetime.strptime(to_date, "%Y-%m-%d")
+
+    # Convert public holidays to a set of datetime objects
+    if public_holidays:
+        public_holidays = set(datetime.strptime(date.strip(), "%Y-%m-%d") for date in public_holidays.split(","))
+    else:
+        public_holidays = set()
 
      # Get a list of all branches
     branches = frappe.get_all("Branch", fields=["branch"])
@@ -83,8 +90,9 @@ def get_data(from_date, to_date, selected_branch):
                     FROM `tabDaily Workforce`
                     WHERE `branch` = %s AND `date` BETWEEN %s AND %s
                     AND DAYOFWEEK(`date`) BETWEEN 2 AND 6  -- Monday (2) to Friday (6)
+                    AND `date` NOT IN ({public_holidays_str})
                 )
-            """, (branch, from_date, to_date))[0][0]
+            """, (branch_name, from_date, to_date))[0][0]
             
             total_off = frappe.db.sql("""
                 SELECT SUM(`employee_off`)
