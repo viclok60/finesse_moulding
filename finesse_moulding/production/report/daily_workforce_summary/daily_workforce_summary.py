afa5b72eb966df7c3cd605d31adccbde5d53b2d6
Victor Lok <121997329+viclok60@users.noreply.github.com>
2024-07-01 12:24:25 +0800

    Update daily_workforce_summary.py

--- finesse_moulding/production/report/daily_workforce_summary/daily_workforce_summary.py
+++ finesse_moulding/production/report/daily_workforce_summary/daily_workforce_summary.py
@@ -52,7 +52,6 @@ def get_data(from_date, to_date, selected_branch, public_holiday_dates):
     # Convert date strings to datetime objects
     from_date = datetime.strptime(from_date, "%Y-%m-%d")
     to_date = datetime.strptime(to_date, "%Y-%m-%d")
-    public_holiday_dates = datetime.strptime(public_holiday_dates, "%Y-%m-%d")
 
      # Get a list of all branches
     branches = frappe.get_all("Branch", fields=["branch"])
