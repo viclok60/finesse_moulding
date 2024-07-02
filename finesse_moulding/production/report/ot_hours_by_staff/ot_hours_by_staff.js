fc709aed70d3aa413a2ee7cbefc7604a9ba11458
Victor Lok <121997329+viclok60@users.noreply.github.com>
2024-07-02 12:05:41 +0800

    Update ot_hours_by_staff.js

--- finesse_moulding/production/report/ot_hours_by_staff/ot_hours_by_staff.js
+++ finesse_moulding/production/report/ot_hours_by_staff/ot_hours_by_staff.js
@@ -2,8 +2,52 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["OT Hours by Staff"] = {
-	"filters": [
-
-	]
+frappe.query_reports["Supervisor OT Hours"] = {
+    "filters": [
+        {
+            "fieldname": "from_selected_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.month_start(),
+            "reqd": 1,
+        },
+        {
+            "fieldname": "to_selected_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.month_end(),
+            "reqd": 1,
+        },
+        {
+            "fieldname": "designation",
+            "label": __("Position"),
+            "fieldtype": "MultiSelect",
+            "options": ["", "Chief Supervisor", "Supervisor", "Assistant Supervisor", "Junior Supervisor", "General Worker"],
+            "on_change": function() {
+                // Trigger the report refresh when the 'branch' filter changes
+                frappe.query_report.refresh();
+            }
+        },
+    ],
+    // Add a custom method to get the selected 'branch' value and date range
+    get_query: function (filters) {
+        return {
+            filters: [
+                {
+                    fieldname: 'designation',
+                    operator: 'like',
+                    value: filters.designation
+                },
+                {
+                    fieldname: 'date',
+                    operator: 'between',
+                    value: [
+                        filters.from_selected_date,
+                        filters.to_selected_date
+                    ]
+                }
+            ]
+        };
+    }
 };
+
