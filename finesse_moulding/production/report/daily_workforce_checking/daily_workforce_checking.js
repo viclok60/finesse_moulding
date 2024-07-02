ea63c0765263157a22660096b7d870aa77550c5b
Victor Lok <121997329+viclok60@users.noreply.github.com>
2024-07-02 12:14:43 +0800

    Update daily_workforce_checking.js

--- finesse_moulding/production/report/daily_workforce_checking/daily_workforce_checking.js
+++ finesse_moulding/production/report/daily_workforce_checking/daily_workforce_checking.js
@@ -4,6 +4,10 @@
 
 frappe.query_reports["Daily Workforce Checking"] = {
 	"filters": [
-
-	]
-};
+        {
+            "fieldname": "date",
+            "label": __("Date"),
+            "fieldtype": "Date",
+            "reqd": 1,
+            "default": frappe.datetime.get_today()
+        },
