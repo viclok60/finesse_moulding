4d8c99cb11a461946d8c6e1cad7b765eb807cca0
Victor Lok <121997329+viclok60@users.noreply.github.com>
2024-07-02 09:16:24 +0800

    Update daily_workforce_summary.py

--- finesse_moulding/production/report/daily_workforce_summary/daily_workforce_summary.py
+++ finesse_moulding/production/report/daily_workforce_summary/daily_workforce_summary.py
@@ -82,6 +82,7 @@ def get_data(from_date, to_date, selected_branch):
                     SELECT `name`
                     FROM `tabDaily Workforce`
                     WHERE `branch` = %s AND `date` BETWEEN %s AND %s
+                    AND `date` != '2024-06-12'
                     AND DAYOFWEEK(`date`) BETWEEN 2 AND 6  -- Monday (2) to Friday (6)
                 )
             """, (branch, from_date, to_date))[0][0]
