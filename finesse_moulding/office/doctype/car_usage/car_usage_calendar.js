0ea8d14a869aab46714b712686c8a5467ea7cba9
viclok60 <viclok118@gmail.com>
2024-07-02 17:10:21 +0800

    update

---
+++ finesse_moulding/office/doctype/car_usage/car_usage_calendar.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2024, Victor and contributors
+// For license information, please see license.txt
+
+frappe.views.calendar["Car Usage"] = {
+	field_map: {
+		start: "time_out",
+		end: "time_in",
+		id: "name",
+		title: "car_model",
+#		allDay: "allDay",
+	},
+	gantt: true,
+	get_events_method: "frappe.desk.calendar.get_events",
+};
